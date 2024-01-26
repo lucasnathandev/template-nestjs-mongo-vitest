f639ae1a823ed3a27250d026f56530227fadc92b
Lucas Nathan Henrique da Silva <lucas.develop.prog@gmail.com>
2024-01-26 00:45:31 -0300

    fix: CORS was disabled and ConfigModule was not imported

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,11 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Injectable, Logger, OnModuleInit } from "@nestjs/common";
+import { PrismaClient } from "@prisma/client";
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  private readonly logger: Logger = new Logger(PrismaService.name);
   async onModuleInit() {
     await this.$connect();
+    this.logger.log("Connected to database");
   }
 }
